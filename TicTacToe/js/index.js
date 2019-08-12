4cb39fbe43dc3ddf448e0bb6b8ecad2d6d74233c
RusbelthCV <49898288+RusbelthCV@users.noreply.github.com>
2019-08-13 01:33:56 +0200

    TicTacToe acabado

--- TicTacToe/js/index.js
+++ TicTacToe/js/index.js
@@ -1,12 +1,22 @@
 $(document).ready(function () {
     var posicion;
-    var tablero = (0,0,0,0,0,0,0,0,0);
+    var celda;
+    var tablero = new Array(0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-    
+
     $(".jugada").css("display", "none");
-    $(".fondo").click(function () {
+    $(".celda").click(function () {
         posicion = $(this);
-        posicionJugada();
+        celda = $(".celda").index($(this));
+        $(".celda").css("border","none");
+        if (tablero[celda] == 0) {
+            posicionJugada();
+            posicion.css("border","1px solid red");
+        }
+        else
+        {
+            $(".jugada").css("display", "none");
+        }
     });
 
     $(".jugada").click(function () {
@@ -14,7 +24,18 @@ $(document).ready(function () {
         let valor = $(this).attr("alt");
         posicion.addClass(valor);
         posicion.removeClass("fondo");
-        $(".jugada").css("display","none");
+        $(".celda").css("border","none");
+        if (valor == "jugadorCruz") {
+            tablero[celda] = 1;
+        }
+        else {
+            tablero[celda] = 2;
+        }
+
+
+
+        ganador(tablero);
+        $(".jugada").css("display", "none");
         if (jugador == "Jugador 1") {
             $("#jugador").text("Jugador 2");
         }
@@ -23,6 +44,34 @@ $(document).ready(function () {
         }
     });
 });
+
+
+
+function ganador(tablero) {
+    var ganador = false;
+
+    if (tablero[0] == 1 && tablero[1] == 1 && tablero[2] == 1 || tablero[3] == 1 && tablero[4] == 1 && tablero[5] == 1 || tablero[6] == 1 && tablero[7] == 1 && tablero[8] == 1 || tablero[0] == 1 && tablero[3] == 1 && tablero[6] == 1 || tablero[1] == 1 && tablero[4] == 1 && tablero[7] == 1 || tablero[2] == 1 && tablero[5] == 1 && tablero[8] == 1 || tablero[0] == 1 && tablero[4] == 1 && tablero[8] == 1 || tablero[2] == 1 && tablero[4] == 1 && tablero[6] == 1) {
+        alert("Ha ganado el jugador 1 !!!");
+        ganador = true;
+    }
+    else if (tablero[0] == 2 && tablero[1] == 2 && tablero[2] == 2 || tablero[3] == 2 && tablero[4] == 2 && tablero[5] == 2 || tablero[6] == 2 && tablero[7] == 2 && tablero[8] == 2 || tablero[0] == 2 && tablero[3] == 2 && tablero[6] == 2 || tablero[1] == 2 && tablero[4] == 2 && tablero[7] == 2 || tablero[2] == 2 && tablero[5] == 2 && tablero[8] == 2 || tablero[0] == 2 && tablero[4] == 2 && tablero[8] == 2 || tablero[2] == 2 && tablero[4] == 2 && tablero[6] == 2) {
+        alert("Ha ganado el jugador 2 !!!");
+        ganador = true;
+    }
+
+    if (ganador) {
+        $(".celda").addClass("fondo");
+        $(".celda").removeClass("jugadorCruz");
+        $(".celda").removeClass("jugadorCirculo");
+        ganador = false;
+        $.each(tablero,function(index,value)
+        {
+            tablero[index] = 0;
+        });
+        console.log(tablero);
+    }
+}
+
 
 
 
